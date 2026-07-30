faf4d9e0fa6a2cb48d6cf89d2e96f912dc2e9ceb
KaritoEscalera <clauditaescalera@gmail.com>
2026-07-30 17:03:10 -0600

    Restaura impresion estable de tickets

--- android/app/src/main/java/com/abarroteselpedernal/app/BluetoothPrinterPlugin.java
+++ android/app/src/main/java/com/abarroteselpedernal/app/BluetoothPrinterPlugin.java
@@ -4,9 +4,6 @@ import android.Manifest;
 import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothDevice;
 import android.bluetooth.BluetoothSocket;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
-import android.graphics.Color;
 import android.os.Build;
 
 import com.getcapacitor.JSObject;
@@ -88,11 +85,6 @@ public class BluetoothPrinterPlugin extends Plugin {
                     socket.connect();
                     OutputStream output = socket.getOutputStream();
                     output.write(new byte[] { 0x1B, 0x40 });
-                    try {
-                        printLogo(output);
-                    } catch (Exception ignored) {
-                        output.write(new byte[] { 0x1B, 0x61, 0x00 });
-                    }
                     output.write(text.getBytes(Charset.forName("CP850")));
                     output.write(new byte[] { 0x0A, 0x0A, 0x0A });
                     output.flush();
@@ -111,59 +103,6 @@ public class BluetoothPrinterPlugin extends Plugin {
                 }
             }).start()
         );
-    }
-
-    private void printLogo(OutputStream output) throws Exception {
-        Bitmap logo = BitmapFactory.decodeResource(getContext().getResources(), R.drawable.ticket_logo);
-        if (logo == null) return;
-
-        int width = logo.getWidth();
-        int height = logo.getHeight();
-        int bytesPerRow = (width + 7) / 8;
-        byte[] image = new byte[bytesPerRow * height];
-        double[][] luminance = new double[height][width];
-
-        for (int y = 0; y < height; y++) {
-            for (int x = 0; x < width; x++) {
-                int pixel = logo.getPixel(x, y);
-                if (Color.alpha(pixel) < 96) {
-                    luminance[y][x] = 255;
-                } else {
-                    luminance[y][x] =
-                        (Color.red(pixel) * 0.299)
-                        + (Color.green(pixel) * 0.587)
-                        + (Color.blue(pixel) * 0.114);
-                }
-            }
-        }
-
-        for (int y = 0; y < height; y++) {
-            for (int x = 0; x < width; x++) {
-                double oldValue = Math.max(0, Math.min(255, luminance[y][x]));
-                double newValue = oldValue < 170 ? 0 : 255;
-                double error = oldValue - newValue;
-                if (newValue == 0) {
-                    image[(y * bytesPerRow) + (x / 8)] |= (byte) (0x80 >> (x % 8));
-                }
-                if (x + 1 < width) luminance[y][x + 1] += error * 7 / 16;
-                if (y + 1 < height) {
-                    if (x > 0) luminance[y + 1][x - 1] += error * 3 / 16;
-                    luminance[y + 1][x] += error * 5 / 16;
-                    if (x + 1 < width) luminance[y + 1][x + 1] += error / 16;
-                }
-            }
-        }
-
-        output.write(new byte[] { 0x1B, 0x61, 0x01 });
-        output.write(new byte[] {
-            0x1D, 0x76, 0x30, 0x00,
-            (byte) (bytesPerRow & 0xFF),
-            (byte) ((bytesPerRow >> 8) & 0xFF),
-            (byte) (height & 0xFF),
-            (byte) ((height >> 8) & 0xFF)
-        });
-        output.write(image);
-        output.write(new byte[] { 0x0A, 0x1B, 0x61, 0x00 });
     }
 
     private BluetoothDevice findPrinter(Set<BluetoothDevice> devices, String requestedName) {
